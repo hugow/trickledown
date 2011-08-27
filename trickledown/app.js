d4ccfc396c5b7fa510cd4cbb9ee057065891893f
hugow <hwindisch@bluestreaktech.com>
2011-08-26 22:31:43 -0400

    Added a minimalistic simulation engine that can be loaded from mongo and that can run from memory.

--- trickledown/app.js
+++ trickledown/app.js
@@ -3,36 +3,59 @@
  * Module dependencies.
  */
 
-var express = require('express');
+/*globals module, __dirname */
 
-var app = module.exports = express.createServer();
+var express = require('express'),
+    mongodb = require('mongodb'),
+    async = require('async'),
+    Db = mongodb.Db,
+    Server = mongodb.Server,
+    client = new Db('trickledown', new Server("127.0.0.1", 27017, {})),
+    World = require('./simulation/engine').World,
+    app = module.exports = express.createServer();
 
 // Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.configure(function () {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+app.configure('development', function () {
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
+app.configure('production', function () {
+    app.use(express.errorHandler());
 });
 
 // Routes
-
-app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
-  });
+app.get('/', function (req, res) {
+    res.render('index', {
+        title: 'Express'
+    });
 });
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+// open the db once
+client.open(function (err) {
+    if (err) {
+        console.log('failed to create db, exiting');
+    } else {
+        var worlds = [ 'odov', 'opov' ];
+        async.map(worlds, function (world, callback) {
+            World.createFromDb(client, world, callback);
+        }, function (err, worlds) {
+            if (err) {
+                console.log('failed to load worlds');
+            }
+            // we need to
+            // start the server
+            app.listen(3000);
+            console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+        });
+    }
+});
+
--- trickledown/simulation/engine.js
+++ trickledown/simulation/engine.js
@@ -1,24 +1,105 @@
-var mongodb = require('mongodb');
-    Db = mongodb.Db,
-    Server = mongodb.Server;
-/*var client = new Db('test', new Server("127.0.0.1", 27017, {})),
-    test = function (err, collection) {
-      collection.insert({a:2}, function(err, docs) {
+// The only thing that will need to be in the database, hopefully,
+// will be:
+// users (username, hashed password, pid1, pid2)
+// playersfunction
 
-        collection.count(function(err, count) {
-          console.log(count);
+// javascript illeteracy: there should be a standard way to do this (obj.keys().forEach ???)
+function forEachProperty(object, f) {
+    var v;
+    for (v in object) {
+        if (object.hasOwnProperty(v)) {
+            f(object[v], v, object);
+        }
+    }
+}
+//      -
+// + the simulation will run in memory
+// + the db will be updated once in a while (we only use it as a big object
+//      store, not really to make queries... as far a the simulation is concerned)
+// (we can recreate the world from the db but run it in memory)
+
+// this is a player's portfolio (stocks he owns, stuff)
+function Portfolio(o) {
+    var that = this;
+    forEachProperty(o, function (value, prop) {
+    });
+}
+
+// this is a game player (i.e not a user but a player)
+function Player(o) {
+    var that = this;
+    if (o === undefined) {
+        o = {
+            world: 'default',
+            howToSpend: {
+                goods: 0.5,
+                education: 0.4,
+                stocks: 0.1,
+                savings: 0
+            },
+            vote: {
+                // how much the government should collect from the rich (and redistribute equally)
+                taxTheRich: 0.01,
+                // how much the government should collect from the poor (and redistribute equally)
+                taxThePoor: 0.1,
+                // what portion of the total taxes should go to corporations (i.e. reverse robin hood)
+                redistributeToCorporations: 0.2
+            },
+            education: 0,
+            cash: 0,
+            income: 0,
+            savings: 0,
+            portfolio: {
+                // investment priorities: how we want to spend our money
+                priorities: {
+                },
+                // stocks: what we have purchased
+                stocks: {
+                }
+            }
+        };
+    }
+    // keep the id if it is defined
+    forEachProperty(o, function (value, prop) {
+        that[prop] = value;
+    });
+    // override the portfolio
+    this.portfolio = new Portfolio(this.portfolio);
+    if (o._id) {
+        this.id = o._id;
+    }
+}
+
+// this is a world
+function World(worldName) {
+    this.worldName = worldName;
+    this.population = [];
+}
+World.prototype.snapShotToDb = function (db, callback) {
+
+};
+// note: this will only be called at the startup when the world is
+// loading (no simulation while loading)
+World.createFromDb = function (db, worldName, callback) {
+    // create an empty world
+    var that = new World(worldName);
+
+    db.collection('players', function (err, coll) {
+        if (err) {
+            return callback(err);
+        }
+        // find all objects in the specified world
+        coll.find({ world: worldName }, function (err, cursor) {
+            cursor.each(function (err, player) {
+                // we are done
+                if (player === null) {
+                    return callback(null, that);
+                }
+                // otherwise, create a player and add it
+                that.population.add(new Player(player));
+            });
         });
+    });
+};
 
-        // Locate all the entries using find
-        collection.find().toArray(function(err, results) {
-            console.log(results);
-          // Let's close the db
-          client.close();
-        });
-      });
-    };
-
-client.open(function(err, p_client) {
-  client.collection('test_insert', test);
-});*/
-
+exports.World = World;
